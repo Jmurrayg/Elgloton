0d94a5f8f5b523776d5a457501f605bec016568f
James Murray <jmurrayg@hotmail.com>
2018-08-30 21:02:41 -0700

    segundo commit

--- JS/js.js
+++ JS/js.js
@@ -14,6 +14,13 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
 }
-
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
 
 // AIzaSyCDDGfrhXGi7V8Cc7fcwF3d2_LrQQf4AVg
